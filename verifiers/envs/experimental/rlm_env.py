4a47b84bed0e758641ee492e5d1b80035ddac4a2
eligotts <78387377+eligotts@users.noreply.github.com>
2026-01-22 09:02:16 -0800

    pass **kwargs through to env_response

--- verifiers/envs/experimental/rlm_env.py
+++ verifiers/envs/experimental/rlm_env.py
@@ -2658,9 +2658,9 @@ class RLMEnv(SandboxEnv):
             # Subsequent turns: use parent implementation
             return await super().get_prompt_messages(state)
 
-    async def env_response(self, messages: Messages, state: State) -> Messages:                                                                                                                 
-      """Override to set final_env_response when answer is ready to avoid extra model call"""                                                                                                                          
-      tool_messages = await super().env_response(messages, state)                                                                                                                             
+    async def env_response(self, messages: Messages, state: State, **kwargs) -> Messages:
+      """Override to set final_env_response when answer is ready to avoid extra model call"""
+      tool_messages = await super().env_response(messages, state, **kwargs)
       if "final_answer" in state:                                                                                                                       
           state["final_env_response"] = tool_messages                                                                                                                                         
       return tool_messages
